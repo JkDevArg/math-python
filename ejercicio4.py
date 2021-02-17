c2aff281f0d6174d9f712a32bd584fcb9e2387eb
JkDev <69984125+JkDevArg@users.noreply.github.com>
2021-02-16 21:39:15 -0300

    Create ejercicio4.py

---
+++ ejercicio4.py
@@ -0,0 +1,5 @@
+#Ejercicio 4.
+#Escribí un programa que solicite al usuario ingresar la cantidad de kilómetros recorridos por una motocicleta y la cantidad de litros de combustible que consumió durante ese recorrido. Mostrar el consumo de combustible por kilómetro.
+kilometros=float(input("Kilometros Recorridos:"))
+litros=float(input("Litros Combustible Consumidos:"))
+print("El Consumo de Litros por Kilometros es de:", kilometros/litros)
