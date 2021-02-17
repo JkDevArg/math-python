0d52ed9a3253e0dbc2ab7b2d3f6a907c0acd3dbc
JkDev <69984125+JkDevArg@users.noreply.github.com>
2021-02-16 21:32:28 -0300

    Create erjecicio1.py

---
+++ erjecicio1.py
@@ -0,0 +1,4 @@
+#Ejercicio 1.
+#Escribí un programa que solicite al usuario que ingrese su nombre. El nombre se debe almacenar en una variable llamada nombre. A continuación se debe mostrar en pantalla el texto “Ahora estás en la matrix, [usuario]”, donde “[usuario]” se reemplazará por el nombre que el usuario haya ingresado.
+nombre=input("Tu nombre:")
+print("Ahora estás en la matrix,", nombre)
